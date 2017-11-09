a0d2014ca646f221195a188682d614c34171109b
mehmet baran <mehmetbaran@mehmetbaran.net>
2017-11-09 19:19:55 +0300

    Issue #1 Open window like mobile view

--- YouTubePlayer/MainForm.Designer.cs
+++ YouTubePlayer/MainForm.Designer.cs
@@ -44,7 +44,7 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(824, 417);
+            this.panel1.Size = new System.Drawing.Size(444, 675);
             this.panel1.TabIndex = 0;
             // 
             // notifyIcon1
@@ -85,7 +85,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(824, 417);
+            this.ClientSize = new System.Drawing.Size(444, 675);
             this.Controls.Add(this.panel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainForm";
