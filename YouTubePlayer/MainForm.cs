27fcf7b92aa5fa57d0607b5ceccc039af8de3608
mbrn <mehmetbaran@mehmetbaran.net>
2019-12-31 14:58:24 +0300

    user agent changed

--- YouTubePlayer/MainForm.cs
+++ YouTubePlayer/MainForm.cs
@@ -41,6 +41,7 @@ namespace YouTubePlayer
             CefSettings settings = new CefSettings();
             //settings.BrowserSubprocessPath = @"x86\CefSharp.BrowserSubprocess.exe";
             settings.CachePath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\CEF";
+            settings.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:68.0) Gecko/20100101 Firefox/68.0";
             settings.PersistSessionCookies = true;
             Cef.Initialize(settings);
             //Cef.Initialize(settings, performDependencyCheck: false, browserProcessHandler: null);
